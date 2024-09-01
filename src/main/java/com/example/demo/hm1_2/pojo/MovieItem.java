36b39ae7990194a8123639a98a136374003ce437
xyzhang <79035486+shedskin516@users.noreply.github.com>
2024-08-31 21:14:05 -0700

    update JsonProperty

--- src/main/java/com/example/demo/hm1_2/pojo/MovieItem.java
+++ src/main/java/com/example/demo/hm1_2/pojo/MovieItem.java
@@ -9,7 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MovieItem {
-    private String Title;
+    @JsonProperty("Title")
+    private String title;
     @JsonProperty("Year")
     private int year;
     private String imdbID;
